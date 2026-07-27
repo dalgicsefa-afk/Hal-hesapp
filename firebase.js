06d211cadc58c903d341aca1d8731e0507ad4df6
dalgicsefa-afk <dalgicsefa@gmail.com>
2026-07-27 14:51:40 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyDJxYtgMfPRiaDxabLRSnmvNJL5JjdzwD0",
   authDomain: "komisyoncep.firebaseapp.com",
@@ -11,7 +7,7 @@ const firebaseConfig = {
   appId: "1:789546692165:web:57acba9cafb4418f6bfd2c"
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const db = firebase.firestore();
+const auth = firebase.auth();
